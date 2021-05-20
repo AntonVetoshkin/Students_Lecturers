81192a59f614cdc4d4086849ddd0b60b234675b8
AntonVetoshkin <aveto77@mail.ru>
2021-05-20 21:32:37 +0500

    First dish fixing

---
+++ Dieshes/dishes.py
@@ -0,0 +1,25 @@
+with open('recipies.txt', encoding='UTF-8') as f:
+    book = {}
+    book.setdefault(f.readline().strip('\n'))
+    print(book)
+    count = int(f.readline())
+    dish_list = []
+
+    for i in range(1, (count + 1)):
+        a = (f.readline().strip('\n').split('|'))
+        c = int(a[1])
+        a[1] = c
+        b = ('ingredient_name', 'quantity', 'measure')
+        dish_list.append(dict(zip(b, a)))
+    print(dish_list)
+
+
+    # c = 0
+    # for line in f:
+    #     if line == '\n':
+    #         c += 1
+    # dish_quantity = c + 1
+    # print(dish_quantity)
+
+
+
