4a368fd72d12012448a3c8822eacde70745046d9
AntonVetoshkin <68805495+AntonVetoshkin@users.noreply.github.com>
2021-05-20 22:39:45 +0500

    Update dishes.py

--- Dieshes/dishes.py
+++ Dieshes/dishes.py
@@ -1,6 +1,6 @@
 with open('recipies.txt', encoding='UTF-8') as f:
-    book = {}
-    book.setdefault(f.readline().strip('\n'))
+    cook_book = {}
+    book = f.readline().strip('\n')
     print(book)
     count = int(f.readline())
     dish_list = []
@@ -10,7 +10,8 @@ with open('recipies.txt', encoding='UTF-8') as f:
         a[1] = int(a[1])
         b = ('ingredient_name', 'quantity', 'measure')
         dish_list.append(dict(zip(b, a)))
-    print(dish_list)
+    cook_book[book] = dish_list
+    print(cook_book)
 
 
     # c = 0
@@ -19,6 +20,7 @@ with open('recipies.txt', encoding='UTF-8') as f:
     #         c += 1
     # dish_quantity = c + 1
     # print(dish_quantity)
+
 
 
 
