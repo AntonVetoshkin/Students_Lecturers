9e8a4c421945811d40348fff749a5b83bdd55782
AntonVetoshkin <aveto77@mail.ru>
2021-05-25 13:36:28 +0500

    Исправленный код, с помощью Бардина Александра

--- Dieshes/dishes.py
+++ Dieshes/dishes.py
@@ -1,5 +1,5 @@
 with open('recipies.txt', encoding='UTF-8') as file_work:
-    cook_dict = {}
+    cook_book = {}
     for line in file_work:
         dish_name = line.strip('\n')
         counter = int(file_work.readline())
@@ -8,23 +8,36 @@ with open('recipies.txt', encoding='UTF-8') as file_work:
             temp_dict = {}
             ingridient = file_work.readline().strip('\n').split('|')
             ingridient[1] = int(ingridient[1])
-            keys = ('ingredient_name', 'quantity', 'measure')
+            keys = ('ingridient_name', 'quantity', 'measure')
             temp_dict.update(dict(zip(keys, ingridient)))
             list_of_ingridient.append(temp_dict)
-        cook_dict[dish_name] = list_of_ingridient
+        cook_book[dish_name] = list_of_ingridient
         file_work.readline()
-
-
 def get_shop_list_by_dishes(dishes, person_count):
-    order = {}
+    shop_list = {}
     for dish in dishes:
-        for dish_name, recipe in cook_dict.items():
-            if dish in dish_name:
-                order_temp = {}
-                for ingr in recipe:
-                    order_temp.update({(ingr['ingredient_name']): {ingr['quantity']*person_count, ingr['measure']}})
-                order.update(order_temp)
-    print(order)
+        if dish in cook_book:
+            for ingridient in cook_book[dish]:
+                new_shop_list_item = dict(ingridient)
+                new_shop_list_item['quantity'] *= person_count
+                # print(new_shop_list_item)
+                if new_shop_list_item['ingridient_name'] not in shop_list:
+                        shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+                else:
+                        shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+    print(shop_list)
+
+
+
+# def get_shop_list_by_dishes(dishes, person_count):
+#     order = {}
+#     for dish in dishes:
+#         for dish_name, recipe in cook_dict.items():
+#             if dish in dish_name:
+#                 order_temp = {}
+#                 for ingr in recipe:
+#                     order_temp.update({(ingr['ingredient_name']): {ingr['quantity']*person_count, ingr['measure']}})
+#                 order.update(order_temp)
+#     print(order)    это был мой код
 
 get_shop_list_by_dishes(['Запеченный картофель','Омлет'], 2)
-
